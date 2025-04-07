65428b5730bcb0d78646c7ff9d63edf228d95efa
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2025-04-07 16:16:31 +0200

    en

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    $(".navbar").toggleClass("fixed-nav", $(window).scrollTop() > 50);
+  });
+    // Toggle search input visibility when search icon is clicked
+    document.addEventListener('DOMContentLoaded', function(){
+      const searchToggle = document.querySelector('.search-toggle');
+      const searchInputContainer = document.querySelector('.search-input-container');
+      searchToggle.addEventListener('click', function(){
+        searchInputContainer.classList.toggle('active');
+      });
+    });
+
+    $("#articlesSlider").owlCarousel({
+      loop: true,             
+      rtl: true,
+      margin: 15,           
+      nav: false,            
+      dots: true,            
+      autoplay: true,        
+      autoplayTimeout: 3000, 
+      autoplayHoverPause: true, 
+      responsive: {
+        0: {
+          items: 1          
+        },
+        768: {
+          items: 1         
+        },
+        992: {
+          items: 1         
+        }
+      }
+    });
+  
+});
